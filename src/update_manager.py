71fa5eac12dc59665e9595d16358b097c50ac684
Kyler Eastridge <felix.wolfz@gmail.com>
2025-08-01 22:13:24 +0100

    Create a stable fake MUUID instead of a random one

--- src/update_manager.py
+++ src/update_manager.py
@@ -307,7 +307,7 @@ def make_VVM_UUID_hash(platform_key):
     if muuid is None:
         #fake it
         log.info("Unable to get system unique id; constructing a dummy")
-        muuid = str(uuid.uuid1())
+        muuid = str(uuid.UUID(bytes=hashlib.md5(uuid.getnode().to_bytes(6)).digest()))
     # hashlib requires a bytes object, not a str
     hash = hashlib.md5(muuid.encode('utf8')).hexdigest()
     return hash
